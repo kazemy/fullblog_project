ac1463a522314e2ee88c9cd8b10db54ceb85e398
Kazemy <a.kazemy@gmail.com>
2020-09-18 17:30:48 -0400

    Added Class base views for CRUD

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 #User table/model is created by django... so we import and will use it
 #in the author field
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -12,3 +13,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # using reverse to get the post instance abolute url
+    def get_absolute_url(self):
+        #first argument is the template name
+        #second is the instance of a specific post primary key
+        return reverse('post-detail', kwargs={'pk':self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
 
 def home(request):
@@ -6,6 +9,71 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    #using existing blog/home.html template instead of django convetion for this one
+    template_name = 'blog/home.html'
+    #adding an extra attribute to identify post object instead of django convention for this one
+    context_object_name = 'posts'
+    #using class based view method on a date_posted field to change display ordering of posts objects
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    #override the form valid mehtod to add author
+    def form_valid(self, form):
+        #get the current form instance author and set it to
+        #the current logged in user
+        form.instance.author = self.request.user
+        #now can validate the form using the parent class
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    #override the form valid mehtod to add author
+    def form_valid(self, form):
+        #get the current form instance author and set it to
+        #the current logged in user
+        form.instance.author = self.request.user
+        #now can validate the form using the parent class
+        return super().form_valid(form)
+    
+    #method to use by UserPassesTestMixin to make sure only author of posts
+    #can update their own posts
+    def test_func(self):
+        # get current post object
+        post = self.get_object()
+        #check if the current logged in user is the post object author atribute
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+
+    #add attribute success_url to forward to blog home after suceessfull deletion
+    success_url = '/'
+
+    #method to use by UserPassesTestMixin to make sure only author of posts
+    #can update their own posts
+    def test_func(self):
+        # get current post object
+        post = self.get_object()
+        #check if the current logged in user is the post object author atribute
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
